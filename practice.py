c220cd267ecdbebba72d5a7023c153738af5125b
btomecsko <brandon.tomecsko92@gmail.com>
2022-08-04 22:04:21 -0500

    add python file

---
+++ practice.py
@@ -0,0 +1,20 @@
+#  First code is printing Hello World
+
+print ('Hello World')
+
+#playing with variables
+
+first_name = "Brandon"
+country = "United States"
+state = "Texas"
+age = 30
+person_info ={
+    'firstname': "Brandon",
+    'country': "United States",
+    'state': "Texas"
+}
+
+#printing the values 
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
